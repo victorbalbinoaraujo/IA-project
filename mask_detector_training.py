8ef01fa25d162df2d5f6c16138065d74336551cf
Victor BAlbino <victorbalbinoaraujo1@outlook.com>
2022-11-23 19:27:54 -0300

    IA Project

---
+++ mask_detector_training.py
@@ -0,0 +1,93 @@
+import os
+import cv2
+import numpy as np
+
+import imutils
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+
+from tensorflow import *
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.layers import AveragePooling2D, Dropout, Flatten, Dense, Input
+from tensorflow.keras.preprocessing.image import img_to_array, load_img, ImageDataGenerator
+
+
+INIT_LR = 1e-4
+EPOCHS = 3
+BS = 32
+
+PATH = r"dataset"
+CATEGORIES = ["with_mask", "without_mask"]
+
+data   = []
+labels = []
+
+for category in CATEGORIES:
+    path = os.path.join(PATH, category)
+    for img in os.listdir(path):
+        img_path = os.path.join(path, img)
+        image = load_img(img_path, target_size=(224, 224))
+        image = img_to_array(image)
+        image = preprocess_input(image)
+
+        data.append(image)
+        labels.append(category)
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+labels = to_categorical(labels)
+
+data = np.array(data, dtype="float32")
+labels = np.array(labels)
+
+(trainX, testX, trainY, testY) = train_test_split(data, labels,
+	test_size=0.20, stratify=labels, random_state=42)
+
+aug = ImageDataGenerator(
+	rotation_range=20,
+	zoom_range=0.15,
+	width_shift_range=0.2,
+	height_shift_range=0.2,
+	shear_range=0.15,
+	horizontal_flip=True,
+	fill_mode="nearest")
+
+baseModel = MobileNetV2(weights="imagenet", include_top=False,
+	input_tensor=Input(shape=(224, 224, 3)))
+
+headModel = baseModel.output
+headModel = AveragePooling2D(pool_size=(7, 7))(headModel)
+headModel = Flatten(name="flatten")(headModel)
+headModel = Dense(128, activation="relu")(headModel)
+headModel = Dropout(0.5)(headModel)
+headModel = Dense(2, activation="softmax")(headModel)
+
+model = Model(inputs=baseModel.input, outputs=headModel)
+
+for layer in baseModel.layers:
+	layer.trainable = False
+
+lr_schedule = keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=1e-2,
+    decay_steps=10000,
+    decay_rate=0.9)
+
+opt = keras.optimizers.SGD(learning_rate=lr_schedule)
+model.compile(loss="binary_crossentropy", optimizer=opt,
+	metrics=["accuracy"])
+
+H = model.fit(
+	aug.flow(trainX, trainY, batch_size=BS),
+	steps_per_epoch=len(trainX) // BS,
+	validation_data=(testX, testY),
+	validation_steps=len(testX) // BS,
+	epochs=EPOCHS)
+
+predIdxs = model.predict(testX, batch_size=BS)
+predIdxs = np.argmax(predIdxs, axis=1)
+
+model.save("mask_detector.model", save_format="h5")
